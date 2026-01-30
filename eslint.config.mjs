ee55470f03ac1dd313379fd1da128d2232ff5e6b
abdulrehmangulfaraz <abdulrehmangulfaraz@gmail.com>
2026-01-31 00:35:29 +0500

    Fix ESLint module scope error by using ES module exports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,3 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+module.exports = {
+  extends: ['next/core-web-vitals', 'next/typescript'],
+}
